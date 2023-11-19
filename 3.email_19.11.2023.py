1764cfbaf2b3a81d089a14c4e818c33110c7b89d
navithaabhinaya <139116142+navithaabhinaya@users.noreply.github.com>
2023-11-19 12:39:00 +0530

    Add files via upload

---
+++ 3.email_19.11.2023.py
@@ -0,0 +1,23 @@
+#LAB 2 19/11/2023
+#4.Write a function called is_valid_email ... 
+def is_valid_email(email):
+    if len(email)<=256:
+        if '@' in email:
+            at_position = email.index('@')
+            if at_position > 0:
+                if '.' in email:
+                    dot_index=email.index('.')
+                    if 0 < at_position < len(email) - 1 and at_position < dot_index < len(email) - 1:
+                        if dot_index != -1 and dot_index < len(email) - 1:
+                            if email[0].isalpha() or email[0].isdigit():
+                                return True
+    else:
+        return False
+   
+print("Enter the email Address")
+email_id=input()
+res=is_valid_email(email_id)
+if res:
+    print("Valid email")
+else:
+    print("Invalid email")
